659a22391da1d9df8eec506d77e304cef307108e
Dmitry Bushev <bushevdv@gmail.com>
2012-12-19 16:44:18 +0400

    Add another test

---
+++ test/files/run/t6591_6.scala
@@ -0,0 +1,25 @@
+import scala.language.existentials
+import scala.reflect.runtime.universe._
+import scala.tools.reflect.ToolBox
+import scala.tools.reflect.Eval
+import java.lang.reflect.InvocationTargetException
+
+class O { class I }
+
+class A extends O {
+  val x = new O
+  val code = reify {
+    val v: x.I = ???
+    v
+  }
+  println(showRaw(code))
+}
+
+object Test extends App {
+  type T = a.x.I forSome { val a: A }
+  try {
+    val v: T = (new A).code.eval
+  } catch {
+    case ex: InvocationTargetException if ex.getCause.isInstanceOf[NotImplementedError] =>
+  }
+}
